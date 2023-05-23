fc6a55253f3222bffda65a02165583f4fa11ba81
arielsam567 <Saidemim112104>
2023-05-23 09:18:26 -0300

    first commit

---
+++ js/road.js
@@ -0,0 +1,27 @@
+class Road{
+    constructor(x,width, laneCount = 3){
+        this.x = x;
+        this.width = width; 
+        this.laneCount = laneCount;
+        this.left = x - width/2;
+        this.right = x + width/2;
+        const infinite = 1000000;
+        this.top = -infinite;
+        this.bottom = infinite; 
+    }
+
+    draw(ctx){
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = 'white';
+        ctx.beginPath();
+        ctx.moveTo(this.left, this.top);
+        ctx.lineTo(this.left, this.bottom);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(this.right, this.top);
+        ctx.lineTo(this.right, this.bottom);
+        ctx.stroke();
+    }
+
+}
