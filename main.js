aef68e94a6f15c7f984df414314b77c53ffdc5fb
arielsam567 <Saidemim112104>
2023-05-30 06:22:58 -0300

    ADD

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-const canvas=document.getElementById("myCanvas");
-canvas.width=300;
+const canvas = document.getElementById("myCanvas");
+canvas.width = 300;
 
-const ctx = canvas.getContext('2d'); 
-const road = new Road(canvas.width/2, canvas.width*0.9, 5);
-const car = new Car(road.getLaneCenter(2), 500, 30,50);
+const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9, 5);
+const car = new Car(road.getLaneCenter(2), 200, 30, 50, "IA", 3);
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 1)];
 
 animate();
 
-function animate(){
-    car.update(road.borders);
+function animate() {
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, []);
+  }
 
-    canvas.height=window.innerHeight;
+  car.update(road.borders, traffic);
 
-    ctx.save();
-    ctx.translate(0,-car.y+canvas.height*0.7);
+  canvas.height = window.innerHeight;
 
-    road.draw(ctx);
-    car.draw(ctx);
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
 
-    ctx.restore();
-    requestAnimationFrame(animate);
+  road.draw(ctx);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx, "red");
+  }
+  car.draw(ctx, "blue");
+
+  ctx.restore();
+  requestAnimationFrame(animate);
 }
